17a9ad3110e773f07be7b4ab840a17f23a1686af
mimoniruzzaman <mimoniruzzaman43@gmail.com>
2024-03-14 10:39:33 -0700

    Tailwind Head And Hero Done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // colors: {
+    //   headingColor: "#3ab7bf",
+    //   primaryColor: "#f97316",
+    //   secondary: "#ffffff",
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
